37d978ccd7f1e2d4f9d2506a79c11a6f55450e9d
WhiteLatexs <168267152+WhiteLatexs@users.noreply.github.com>
2026-01-06 21:09:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  ownerID: ["590354442214178856", "896063904415879168"], 
+  mongodbUri : "mongodb+srv://OnlyWhi:<AkunPunyaMahlukPutihIni>@cluster0.w3jds5l.mongodb.net/?appName=Cluster0",
   spotifyClientId : "d92baed9605a45a39ed7c2a2d960b1c1",
   spotifyClientSecret : "e9b29f6739de4315bc03b6d8a8e93b03",
   setupFilePath: './commands/setup.json',
@@ -12,7 +12,7 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/TaRQemxpSG",
   embedTimeout: 5,
   showProgressBar: false,  // Show progress bar in track embed
   showVisualizer: false,  // Show visualizer on music card (disabled for low-memory optimization)
